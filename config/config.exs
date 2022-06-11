77e9f306fe1aa9dfbad761df572be22f4df24b0e
Konstantin Astafurov <konstantinaa@Artyoms-MacBook-Pro-2.local>
2022-06-10 21:46:17 -0400

    dead token

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ import Config
 
 config :nostrum,
   # The token of your bot as a string
-  token: "",
+  token: "OTg0OTg4NTYxOTkwMzY1MTg0.GWfpSW.zeDNfYBtMXT-P7Jakhddi5CgvJP74foezE0LeI",
   gateway_intents: :all
 
 config :qoup,
